7b1d9750125e134f05a9a1826df10d04307b2772
Iheb Garsi <ihebgarsi78@gmail.com>
2024-10-02 10:29:40 +0100

    adding update videos function

--- frontend/src/api/videoAPI.tsx
+++ frontend/src/api/videoAPI.tsx
@@ -1,1 +1,16 @@
 const BASE_URL = "http://localhost:4000/videos";
+
+export const deleteVideo = async (idVideo: string) => {
+  try {
+    const response = await fetch(`${BASE_URL}/updatedVideo/${idVideo}`, {
+      method: "DELETE",
+    });
+    if (!response.ok) {
+      throw new Error(`error in deleting the video ,${response}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
