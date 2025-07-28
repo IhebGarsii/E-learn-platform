06ebbdad5c79497e09ec601c9e38243986328350
Iheb Garsi <ihebgarsi78@gmail.com>
2025-07-28 14:37:09 +0100

    fixing video update

--- frontend/src/components/videoUpload/UpdateVideo.tsx
+++ frontend/src/components/videoUpload/UpdateVideo.tsx
@@ -1,34 +1,27 @@
 import { useEffect, useState } from "react";
 import { fullVideo } from "../../types/video";
 import { Link, useParams } from "react-router-dom";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { MdDelete, MdOutlineOndemandVideo } from "react-icons/md";
 import img from "../../../public/arrow-dwon.png";
 import { cousers } from "../../types/course";
 import { addVideo, deleteVideo } from "../../api/videoAPI";
 import { useCourseState } from "../../state/course";
+import { getCourse } from "../../api/coursesAPI";
 
 function UpdateVideo() {
   const { idCourse } = useParams();
   const queryClient = useQueryClient();
-  const [videos, setVideo] = useState<fullVideo>();
   const [addingVideo, setAddingVideo] = useState(true); //change this to true after testing
   const [videoFile, setVideoFile] = useState<File>();
-  let course: cousers;
-  const { setData } = useCourseState();
 
-  useEffect(() => {
-    course = queryClient.getQueryData(["course", idCourse])!;
+  const { data: course } = useQuery({
+    queryKey: ["course", idCourse],
+    queryFn: () => getCourse(idCourse!),
+    enabled: !!idCourse,
+  });
+  console.log(course.video);
 
-    setVideo(course.video);
-    console.log(course.video);
-
-    if (videos) {
-      console.log("Video found in cache:", videos);
-    } else {
-      console.log("video not found in cache, fetching...", videos);
-    }
-  }, []);
   const { mutate: mutateDelete } = useMutation({
     mutationFn: ({
       idVideos,
@@ -44,8 +37,6 @@ function UpdateVideo() {
       queryClient.invalidateQueries({
         queryKey: ["course", idCourse],
       });
-      setData(data);
-      setVideo(data.video);
     },
   });
 
@@ -55,6 +46,8 @@ function UpdateVideo() {
     idVideo: string
   ) => {
     // Pass all parameters as a single object
+    console.log(idVideos, idSection, idVideo);
+
     mutateDelete({ idVideos, idSection, idVideo });
   };
   const handleVideoFileChange = (
@@ -90,8 +83,8 @@ function UpdateVideo() {
 
   return (
     <div className="border-2 border-gray-300 py-2  ">
-      {videos &&
-        videos.video.map((vid, index) => (
+      {course.video &&
+        course.video.video.map((vid: any, index: number) => (
           <div key={index}>
             <h1 className="flex items-center text-xl font-bold gap-2 cursor-pointer border-b-2 border-gray-300 pl-5 pb-1">
               <img className="w-3" src={img} alt="Toggle dropdown" />
@@ -108,7 +101,9 @@ function UpdateVideo() {
                     onChange={(e) => handleVideoFileChange(e)}
                   />
                   <button
-                    onClick={() => handleSubmitVideo(videos._id, vid._id)}
+                    onClick={() =>
+                      handleSubmitVideo(course.video?._id!, vid._id)
+                    }
                   >
                     submit
                   </button>
@@ -116,7 +111,7 @@ function UpdateVideo() {
               )}
             </h1>
             <ul>
-              {vid.videoList.map((video, vidIndex) => (
+              {vid.videoList.map((video: any, vidIndex: any) => (
                 <div className="flex justify-between p-1 " key={vidIndex}>
                   <Link
                     to={`/Course/${idCourse}/${video._id}/${video.videoName}`}
@@ -126,7 +121,9 @@ function UpdateVideo() {
                   </Link>
                   <button
                     className="bg-red-600 text-white px-4 py-2 justify-center rounded"
-                    onClick={() => handleDelete(videos._id, vid._id, video._id)}
+                    onClick={() =>
+                      handleDelete(course.video?._id!, vid._id, video._id)
+                    }
                   >
                     <MdDelete />
                   </button>
