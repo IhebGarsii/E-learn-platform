5c081518580114a2cc62cc192e54de59479a405f
IhebGarsii <ihebgarsi78@.com>
2025-08-17 21:23:53 +0100

    i

--- backend/controller/cartController.js
+++ backend/controller/cartController.js
@@ -11,10 +11,7 @@ const addToCart = async (req, res) => {
     const cartt = await cartModel
       .findOne({ idUser })
       .populate("courses")
-      .populate({
-        path: "courses.studentsId",
-        model: "userModel",
-      });
+     
     // if new cart has to be created
     if (!cartt) {
       console.log("!cartt");
