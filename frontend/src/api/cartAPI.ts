f8c70f50950650942580a19cbca9f70ddb627ce0
IhebGarsii <ihebgarsi78@gmail.com>
2025-09-12 20:25:08 +0100

    cart api

--- frontend/src/api/cartAPI.ts
+++ frontend/src/api/cartAPI.ts
@@ -13,36 +13,8 @@ export const addToCart = async (idCourse: string, idUser: string) => {
     throw error;
   }
 };
-export const getUserCart = async (idUser: string | null) => {
-  try {
-    const response = await fetch(`${BASE_URL}/getUserCart/${idUser}`);
-    if (!response.ok) {
-      throw new Error();
-    }
-    return await response.json();
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-};
-export const removeFromCart = async (idCourse: string, idCart: string) => {
-  try {
-    const response = await fetch(
-      `${BASE_URL}/removeFromCart/${idCart}/${idCourse}`
-    );
 
-    if (!response.ok) {
-      const errorMessage = `Error: ${response.status} - ${response.statusText}`;
-      throw new Error(errorMessage);
-    }
-
-    return await response.json();
-  } catch (error) {
-    console.error("Failed to remove from cart:", error);
-    throw error;
-  }
-};
-export const applyCoupon = async (coupon: string) => {
+/* export const applyCoupon = async (coupon: string) => {
   try {
     const res = await fetch(`${COUPON_URL}/getCoupon/${coupon}`);
     if (!res.ok) {
@@ -57,3 +29,4 @@ export const applyCoupon = async (coupon: string) => {
 export const updateProductQuantity = async () => {
   
 };
+ */
