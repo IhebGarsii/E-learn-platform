aa0b8771e993fd32b488bf450c4d58913fa2de9c
IhebGarsii <ihebgarsi78@gmail.com>
2025-10-07 15:32:24 +0100

    recomanding algo

--- frontend/src/components/studentAlsoBought/StudentsAlsoBought.tsx
+++ frontend/src/components/studentAlsoBought/StudentsAlsoBought.tsx
@@ -24,18 +24,22 @@ function StudentsAlsoBought({ category }: StudentsAlsoBoughtProps) {
           <div className=" mb-4 text-xs font-semibold p-3 ">
             <div className="flex aline-center gap-3">
               <img
-                className="w-12 b-rounded  "
+                className="w-14 h-14 b-rounded  "
                 src={`http://localhost:4000/uploads/courses/${course.courseId.thumbnail}`}
                 alt=""
               />
               <div>
-                <Link to={`/Course/${course.courseId._id}`}>{course.courseId.title}</Link>
+                <Link to={`/Course/${course.courseId._id}`}>
+                  {course.courseId.title}
+                </Link>
 
                 <h1 className="text-gray-500">
                   {course.courseId.studentsId.length} students
                 </h1>
                 <div className="flex items-center gap-2 text-gray-500 mt-2">
-                  <span className="text-gray-500" >{course.courseId.avgRate.nbRate}</span>
+                  <span className="text-gray-500">
+                    {course.courseId.avgRate.nbRate}
+                  </span>
                   <Rating
                     className="text-xs"
                     style={{ maxWidth: 200, width: 60 }}
@@ -45,9 +49,22 @@ function StudentsAlsoBought({ category }: StudentsAlsoBoughtProps) {
                   <span className="whitespace-nowrap text-gray-500">
                     ({course.courseId.avgRate.nbRate} ratings)
                   </span>
-                  <span> {course.courseId.videoDuration} </span>
-                  <span> {course.courseId.difficultyLevel} </span>
                 </div>
+                <div className="flex items-center gap-2 mt-2">
+                  <span className="text-gray-500">
+                    {" "}
+                    {course.courseId.videoDuration} total hours
+                  </span>
+                  <span className="text-gray-500">
+                    {" "}
+                    {course.courseId.difficultyLevel}{" "}
+                    {course.courseId.timeAccess}
+                  </span>
+                </div>
+                <h1 className="text-2xl font-bold mt-2">
+                ${course.courseId.price}
+
+                </h1>
               </div>
             </div>
           </div>
